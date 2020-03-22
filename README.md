b281a2304a0e6a08ee95fdd930f68b1a917e029b
Brian Pollack <brianleepollack@gmail.com>
2020-03-22 14:54:23 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Covid19 Python Visualizer
+Using holoviews and pandas to make a quick and easy interactive visualizer, specifically for jupyter notebook exploration.
---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(name='covid19_viz',
+                 version='0.1.0',
+                 description='Holoviews Covid Viewer',
+                 long_description=open('README.md').read().strip(),
+                 author='Brian Pollack',
+                 author_email='brp98@pitt.edu',
+                 py_modules=['covid19_viz'],
+                 install_requires=[],
+                 license='MIT License',
+                 zip_safe=False
+                 )
